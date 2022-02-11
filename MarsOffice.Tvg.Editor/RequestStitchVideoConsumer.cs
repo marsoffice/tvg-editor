1b820421525ea133168d06a533c5e7aa6df0c02a
Alin Florin <alin@qoffice.ro>
2022-02-11 20:09:03 +0200

    d

--- MarsOffice.Tvg.Editor/RequestStitchVideoConsumer.cs
+++ MarsOffice.Tvg.Editor/RequestStitchVideoConsumer.cs
@@ -1,16 +1,24 @@
 using System;
 using System.Threading.Tasks;
 using MarsOffice.Tvg.Editor.Abstractions;
+using Microsoft.Azure.Management.Media;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Rest;
+using Microsoft.Rest.Azure.Authentication;
 
 namespace MarsOffice.Tvg.Editor
 {
     public class RequestStitchVideoConsumer
     {
         private readonly IConfiguration _config;
+
+        public RequestStitchVideoConsumer(IConfiguration config)
+        {
+            _config = config;
+        }
 
         [FunctionName("RequestStitchVideoConsumer")]
         public async Task Run(
@@ -20,9 +28,11 @@ namespace MarsOffice.Tvg.Editor
         {
             try
             {
-
+                var client = await CreateMediaServicesClientAsync();
+                client.ApiVersion.ToString();
             } catch (Exception e)
             {
+                log.LogError(e, "Function threw an exception");
                 await stitchVideoResponseQueue.AddAsync(new StitchVideoResponse { 
                     Error = e.Message,
                     JobId = request.JobId,
@@ -33,6 +43,23 @@ namespace MarsOffice.Tvg.Editor
                 });
                 await stitchVideoResponseQueue.FlushAsync();
             }
+        }
+
+        private async Task<ServiceClientCredentials> GetCredentialsAsync()
+        {
+
+            var clientCredential = new ClientCredential(_config["adclientid"], _config["adclientsecret"]);
+            return await ApplicationTokenProvider.LoginSilentAsync(_config["tenantid"], clientCredential, ActiveDirectoryServiceSettings.Azure);
+        }
+
+        private async Task<IAzureMediaServicesClient> CreateMediaServicesClientAsync()
+        {
+            var credentials = await GetCredentialsAsync();
+
+            return new AzureMediaServicesClient(new Uri(_config["armendpoint"]), credentials)
+            {
+                SubscriptionId = _config["subscriptionid"]
+            };
         }
     }
 }
