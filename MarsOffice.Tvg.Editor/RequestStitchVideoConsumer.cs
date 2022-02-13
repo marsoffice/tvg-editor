c9668f69fba1ca919fd14b15b34beaca423dc7eb
Alin Florin <alin@qoffice.ro>
2022-02-13 13:58:02 +0200

    r

--- MarsOffice.Tvg.Editor/RequestStitchVideoConsumer.cs
+++ MarsOffice.Tvg.Editor/RequestStitchVideoConsumer.cs
@@ -1,6 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using MarsOffice.Tvg.Editor.Abstractions;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -10,10 +16,13 @@ namespace MarsOffice.Tvg.Editor
     public class RequestStitchVideoConsumer
     {
         private readonly IConfiguration _config;
+        private readonly CloudBlobClient _blobClient;
 
         public RequestStitchVideoConsumer(IConfiguration config)
         {
             _config = config;
+            var csa = CloudStorageAccount.Parse(config["localsaconnectionstring"]);
+            _blobClient = csa.CreateCloudBlobClient();
         }
 
         [FunctionName("RequestStitchVideoConsumer")]
@@ -22,8 +31,19 @@ namespace MarsOffice.Tvg.Editor
             [Queue("stitch-video-response", Connection = "localsaconnectionstring")] IAsyncCollector<StitchVideoResponse> stitchVideoResponseQueue,
             ILogger log)
         {
+            string tempDirectory = null;
             try
             {
+                tempDirectory = Path.GetTempPath() + "/" + Guid.NewGuid().ToString();
+                Directory.CreateDirectory(tempDirectory);
+                await Task.WhenAll(new[]
+                {
+                    DownloadFile(request.AudioBackgroundFileLink, tempDirectory + "/audiobg.mp3"),
+                    DownloadFile(request.VideoBackgroundFileLink, tempDirectory + "/videobg.mp4"),
+                    DownloadFile(request.AudioBackgroundFileLink, tempDirectory + "/speech.mp3")
+                });
+
+                await AddTextOverlays(request, tempDirectory);
 
             }
             catch (Exception e)
@@ -39,7 +59,58 @@ namespace MarsOffice.Tvg.Editor
                     VideoId = request.VideoId
                 });
                 await stitchVideoResponseQueue.FlushAsync();
+            } finally
+            {
+                try
+                {
+                    if (!string.IsNullOrEmpty(tempDirectory) && Directory.Exists(tempDirectory))
+                    {
+                        Directory.Delete(tempDirectory, true);
+                    }
+                } catch (Exception)
+                {
+                    // ignored
+
+
+
+
+
+
+
+                }
             }
+        }
+
+        private async Task AddTextOverlays(RequestStitchVideo request, string tempDirectory)
+        {
+            await ExecuteFfmpeg("", tempDirectory);
+        }
+
+        private async Task<bool> ExecuteFfmpeg(string arguments, string workingDir)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = _config["ffmpegpath"],
+                Arguments = arguments,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                WorkingDirectory = workingDir
+            };
+
+            var process = Process.Start(psi);
+            process.WaitForExit((int)TimeSpan.FromSeconds(60).TotalMilliseconds);
+            return process.ExitCode != 0;
+        }
+
+        private async Task DownloadFile(string link, string outPath)
+        {
+            var containerReference = _blobClient.GetContainerReference(link.Split("/").First());
+            var fileName = string.Join("/", link.Split("/").Skip(1).ToList());
+            var blobReference = containerReference.GetBlockBlobReference(fileName);
+            using var fileStream = File.OpenWrite(outPath);
+            using var readStream = await blobReference.OpenReadAsync();
+            await readStream.CopyToAsync(fileStream);
         }
     }
 }
