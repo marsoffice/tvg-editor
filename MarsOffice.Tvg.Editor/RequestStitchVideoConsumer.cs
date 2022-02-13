f9655bf2c932563fd1cfad593132bf504f0e820a
Alin Florin <alin@qoffice.ro>
2022-02-13 19:59:31 +0200

    d

--- MarsOffice.Tvg.Editor/RequestStitchVideoConsumer.cs
+++ MarsOffice.Tvg.Editor/RequestStitchVideoConsumer.cs
@@ -136,13 +136,13 @@ namespace MarsOffice.Tvg.Editor
         {
             var to = TimeSpan.FromMilliseconds(request.Durations.Sum());
             var newTs = new TimeSpan(to.Hours, to.Minutes, to.Seconds);
-            var command = $"-i videobg.mp4 -i audio_merged.mp3 -map 0:v -map 1:a -ss 00:00:00 -to {newTs} -y -c:v libx264 -preset ultrafast -vf \"subtitles=subs.srt:force_style='Alignment=10,BackColour=&H{(request.TextBoxOpacity == null ? "80" : ToHex(request.TextBoxOpacity.Value))}000000,BorderStyle=4,Fontsize={request.TextFontSize ?? 24},PrimaryColour=&H{(request.TextColor != null ? request.TextColor.Replace("#", "") : "ffffff")}&'\" -codec:a copy final.mp4";
+            var command = $"-hide_banner -loglevel error -i videobg.mp4 -i audio_merged.mp3 -map 0:v -map 1:a -ss 00:00:00 -to {newTs} -y -c:v libx264 -preset ultrafast -vf \"subtitles=subs.srt:force_style='Alignment=10,BackColour=&H{(request.TextBoxOpacity == null ? "80" : ToHex(request.TextBoxOpacity.Value))}000000,BorderStyle=4,Fontsize={request.TextFontSize ?? 24},PrimaryColour=&H{(request.TextColor != null ? request.TextColor.Replace("#", "") : "ffffff")}&'\" -codec:a copy final.mp4";
             return await ExecuteFfmpeg(command, tempDirectory);
         }
 
         private async Task<bool> MergeAudio(RequestStitchVideo request, string tempDirectory)
         {
-            var command = $"-i audiobg.mp3 -i speech.mp3 -filter_complex \"[1:a]volume=1,apad[A];[0:a]volume={(request.AudioBackgroundVolumeInPercent == null ? 0.1 : Math.Round(request.AudioBackgroundVolumeInPercent.Value / 100d, 2))},[A]amerge[out]\" -c:v copy -map [out] -y audio_merged.mp3";
+            var command = $"-hide_banner -loglevel error -i audiobg.mp3 -i speech.mp3 -filter_complex \"[1:a]volume=1,apad[A];[0:a]volume={(request.AudioBackgroundVolumeInPercent == null ? 0.1 : Math.Round(request.AudioBackgroundVolumeInPercent.Value / 100d, 2))},[A]amerge[out]\" -c:v copy -map [out] -y audio_merged.mp3";
             return await ExecuteFfmpeg(command, tempDirectory);
         }
 
